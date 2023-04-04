37cdf1f4c73d88d5624f85f1b3c02fa503405257
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-04-04 17:17:48 +0530

    center style

--- build-push.bat
+++ build-push.bat
@@ -1,7 +1,7 @@
 @echo off
-SET QUAY_USERNAME=<your-quay-username>
-SET IMAGE_NAME=<your-image-name>
+SET QUAY_USERNAME=arbhatta
+SET IMAGE_NAME=dx-demo
 SET VERSION=1.0.0
-
+docker login --username=$Env:quay_user --password=$Env:quay_token quay.io
 docker build -t quay.io/%QUAY_USERNAME%/%IMAGE_NAME%:%VERSION% .
 docker push quay.io/%QUAY_USERNAME%/%IMAGE_NAME%:%VERSION%
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import HelloName from '../components/HelloName';
-
+import styles from '../styles/helloName.module.css';
 export async function getServerSideProps() {
   const name = process.env.NAME;
   return { props: { name } };
 }
 
 export default function Index({ name }: { name: string }) {
-  return <HelloName name={name} />;
+  return <div className ={styles.center}><HelloName name={name} /></div>;
 }
